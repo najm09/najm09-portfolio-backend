d400f0bb9e1b4958aff01e1d7cc89353426e3066
najm09 <fnajm09@gmail.com>
2023-01-30 18:54:04 +0530

    code refactor

--- Controllers/projects.js
+++ Controllers/projects.js
@@ -2,36 +2,42 @@ const Projects = require("../Models/Projects");
 
 
 const getAllLikes = async (req, res) => {
-  try{
+  try {
     const projectLikes = await Projects.find();
     res.status(200).send(projectLikes);
   }
-  catch(error){
-    res.status(500).send({message:error})
+  catch (error) {
+    res.status(500).send({ message: error })
   }
 }
 
 const Like = async (req, res) => {
   const likes = req.body.likes;
   const title = req.body.title;
-  try{
-    if(likes === 0){
+  const id = req.body.id;
+  try {
+    if (likes === 0) {
       const project = await new Projects({
+        id: id,
         title: title,
         likes: 1
       })
       project.save();
     }
-    else{
-      await Projects.findOneAndUpdate({title: title}, {
-        likes : likes + 1
-      }, {new: true})
+    else {
+      await Projects.findOneAndUpdate({ title: title }, {
+        likes: likes + 1
+      }, { new: true })
     }
-    res.status(200).send({message : `You Liked ${title} !`});
+    res.status(200).send({
+      message: `You Liked ${title} !`,
+      title: title
+    }
+    );
   }
-  catch(error){
-    res.status(500).send({message: error})
+  catch (error) {
+    res.status(500).send({ message: error })
   }
 }
 
-module.exports = {Like, getAllLikes}
+module.exports = { Like, getAllLikes }
--- Models/Projects.js
+++ Models/Projects.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const ProjectSchema = new mongoose.Schema({
+  id: Number,
   title: String,
   likes: Number,
 })
